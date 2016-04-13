ec5a3ff26acd28021951f3480cb7e7361a624af1
bellatoris <bellatoris@snu.ac.kr>
2016-04-13 20:54:46 +0900

    new picture

--- test/sig.c
+++ test/sig.c
@@ -4,21 +4,21 @@
 #include <stdio.h>
 
 void s(int d) {
-      printf("begin (%d)\n", d);
-        sleep(5);
-	  printf("end   (%d)\n", d);
+    printf("begin (%d)\n", d);
+    sleep(5);
+    printf("end   (%d)\n", d);
 }
 
 int main() {
-      int pid = getpid();
-        signal(SIGINT, s);
-	  signal(SIGCHLD, s);
-	    if (fork() == 0) {
-		    kill(pid, SIGINT);
-		        sleep(1);
-			    kill(pid, SIGINT);
-			        sleep(1);
-				    exit(0);
-				      }
-	      for(;;);
+    int pid = getpid();
+    signal(SIGINT, s);
+    signal(SIGCHLD, s);
+    if (fork() == 0) {
+	kill(pid, SIGINT);
+	sleep(1);
+	kill(pid, SIGINT); //signal handler가 불린 순간 signal bit은 0으로 바뀐다.
+	sleep(1);
+	exit(0);
+	}
+    for(;;);
 }
