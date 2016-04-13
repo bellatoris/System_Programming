19ee147ba759439ab47991c5ae235621309c2207
bellatoris <bellatoris@snu.ac.kr>
2016-04-13 21:09:41 +0900

    new picture

---
+++ quiz/fun2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int fd1;
+    int s = 1;
+    char c1, c2;
+    char *fname = argv[1];
+    fd1 = open(fname, O_RDONLY, 0);
+    read(fd1, &c1, 1);
+    if (fork()) {
+	sleep(s);
+	read(fd1, &c2, 1);
+	printf("Parent: c1 = %c, c2 = %c\n", c1, c2);
+    } else {
+	sleep(1 - s);
+	read(fd1, &c2, 1);
+	printf("Child: c1 = %c, c2 = %c\n",c1 ,c2); 
+    }
+    return 0;
+}
